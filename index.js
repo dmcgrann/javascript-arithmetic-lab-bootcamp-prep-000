1c2ba3191af4287bb8c772216e8b887cb98ea0c9
David McGrann <dpmcgrann@gmail.com>
2017-11-26 19:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function dec(a) {
   return(a -= 1)
 }
 
-function makeInt() {
-  return parseInt(a, 10)
+function makeInt(n) {
+  return(parseInt(n, 10))
 }
 
 function makeInt() {
-  preserveDecimal(a)
+  return(preserveDecimal(n))
 }
